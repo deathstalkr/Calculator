6bed1180d4c836110ae6175090859994b9ca903b
deathstalkr <solomon.johnson098@gmail.com>
2024-01-12 09:58:20 +0530

    basic logic and styles

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
-let firstNumber;
-let operator;
-let secondNumber;
+let expression = "";
 
-const CURRENTSELECTION = document.getElementById("user-input");
-const NUMBERS = document.querySelectorAll(".keys.display");
-const EQUALS = document.getElementsByClassName("equals");
-const CLEAR = document.getElementsByClassName("function");
-const DECIMAL = document.getElementsByClassName("decimal");
+const DISPLAY = document.getElementById("user-input");
+const RESULT = document.getElementById("result")
+const buttons = document.querySelectorAll('.keys.display')
 
+function appendToDisplay(value){
+	if (value === '.' && /\.\d*$/.test(expression)) {
+		return;
+	  } else if (isOperator(value)) {
+		expression = expression.replace(/[-+*/%]$/, '')
+	  }
 
-
-
-function operate() {
-	if (operator == "+") add(firstNumber, secondNumber);
-	if (operator == "-") subtract(firstNumber, secondNumber);
-	if (operator == "*") multiply(firstNumber, secondNumber);
-	if (operator == "/") divide(firstNumber, secondNumber);
+	expression += value;
+	updateDisplay(expression); 
 }
 
-NUMBERS.forEach((element) => {
-	element.addEventListener("click", function (e) {
-		num = e.target.value;
-		CURRENTSELECTION.textContent += `${num}`;
+function isOperator(value){
+	return value === '+' || value === '-' || value === '*' || value === '/' || value === '%';
+}
+
+function updateResult(){
+	RESULT.value = expression;
+}
+
+function clearAllDisplay(){
+	expression = "";
+	console.log("expression:", expression)
+	updateResult();
+	updateDisplay("0");
+	enableButtons();
+}
+
+function deleteDisplay(){
+	expression = expression.slice(0, -1);
+	updateDisplay(expression);
+	enableButtons();
+}
+
+function updateDisplay(value) {
+	if (value.includes('*') || value.includes('/')) {
+	  value = value.replace(/\*/g, 'x').replace(/\//g, '÷');
+	}
+	DISPLAY.value = value || '';
+  }
+
+function calculateResult(){
+	const result = math.evaluate(expression) || '0';
+	console.log("result:", result)
+	expression = result.toString();
+	console.log("expression:", expression)
+	updateResult();
+	disableButtons();
+	expression = "";
+}
+
+function disableButtons() {
+	buttons.forEach(button => {
+		button.disabled = true;
 	});
-});
+}
+
+function enableButtons() {
+	buttons.forEach(button => {
+	  button.disabled = false;
+	});
+  }
